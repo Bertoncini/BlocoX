1abe9c92242b51c88c88d0f51771301413196d89
juliano <juliano_braz@hotmail.com>
2019-10-22 12:30:40 -0300

    feat(utils): adiciona classe de utils para tratamentos repetidos

---
+++ BlocoX.Utils/Utils/Utils.cs
@@ -0,0 +1,14 @@
+﻿namespace BlocoX.Utils.Utils
+{
+    public static class Utils
+    {
+        public static string DecimalParaString(this decimal valor)
+        {
+            var stringValor = valor.ToString("n2");
+            stringValor = stringValor.Replace(",", "");
+            stringValor = stringValor.Replace(".", "");
+            return stringValor;
+        }
+
+    }
+}
