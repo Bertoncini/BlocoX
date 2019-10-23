301b4ee7c81d8997f239b8b9138c1f6fc0fd16d2
juliano <juliano_braz@hotmail.com>
2019-10-23 12:57:46 -0300

    feat(informacoes): adiciona novas funcoes para os testes

--- BlocoX.Test/BlocoX.Utils/Fakes/Informacoes.cs
+++ BlocoX.Test/BlocoX.Utils/Fakes/Informacoes.cs
@@ -1,5 +1,6 @@
 ﻿using BlocoX.Modelos;
 using BlocoX.Modelos.ReducaoZ;
+using BlocoX.Utils.Xml;
 using System;
 using System.Collections.Generic;
 
@@ -13,7 +14,20 @@ namespace BlocoX.Test.BlocoXUtils.Fakes
         public static PafEcf ObterPafEcf()
             => new PafEcf("123456789012345");
 
+        public static Ecf ObterECF()
+            => new Ecf("UR010905000");
+
         public static DadosReducaoZ ObterDadosReducaoZ()
-            => new DadosReducaoZ(new DateTime(2019, 01, 01), new DateTime(2019, 01, 01, 12, 59, 59), 1, 100, 1, 1000, 1850, new List<TotalizadorParcial>());
+            => new DadosReducaoZ(new DateTime(2019, 01, 01), new DateTime(2019, 01, 01, 12, 59, 59), 1, 100, 1, 1000, 1850);
+
+        public static TotalizadorParcial ObterTotalizadorParcial()
+            => new TotalizadorParcial("01T1700", 2000m, ObterListaProdutos());
+
+        public static List<ProdutoServico> ObterListaProdutos()
+            => new List<ProdutoServico> { new ProdutoServico("PRODUTO ALIQUOTA 1700", "0", "1400300", "11041900", "220601", 2, "UN", 0M, 0M, 0M, 1000M, false),
+                                          new ProdutoServico("PRODUTO ALIQUOTA 1700", "0", "1400300", "11041900", "220601", 2, "UN", 0M, 0M, 0M, 1000M, false)};
+
+        public static XmlReducaoZ ObterXMlReducaoZ()
+            => new XmlReducaoZ(new BlocoXRZ(ObterECF(), ObterDadosReducaoZ(), new List<TotalizadorParcial> { ObterTotalizadorParcial() }));
     }
 }
