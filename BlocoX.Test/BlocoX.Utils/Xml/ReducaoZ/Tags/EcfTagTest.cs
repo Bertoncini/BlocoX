c6244eb1ad016d4ff95fff964a807cf500218294
juliano <juliano_braz@hotmail.com>
2019-10-23 12:58:55 -0300

    test: adiciona teste de unidade a tag ecf

---
+++ BlocoX.Test/BlocoX.Utils/Xml/ReducaoZ/Tags/EcfTagTest.cs
@@ -0,0 +1,18 @@
+﻿using BlocoX.Test.BlocoXUtils.Fakes;
+using BlocoX.Utils.Xml.ReducaoZ.Tags;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BlocoX.Test.BlocoXUtils.Xml.ReducaoZ.Tags
+{
+    [TestClass]
+    [TestCategory("ECF - Validar informações de ECF")]
+    public class EcfTagTest
+    {
+        [TestMethod]
+        public void Deve_validar_informacoes_ecf()
+        {
+            var ecf = new EcfTag(Informacoes.ObterECF());
+            Assert.AreEqual("<NumeroFabricacao>UR010905000</NumeroFabricacao>", ecf.ObterTag().ToString());
+        }
+    }
+}
