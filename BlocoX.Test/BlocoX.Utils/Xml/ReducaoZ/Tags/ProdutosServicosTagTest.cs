f869ed72ce11fb7a9596a9bcddde01b043266e5b
juliano <juliano_braz@hotmail.com>
2019-10-23 12:59:24 -0300

    test: adiciona teste a tag de produtos servicos

---
+++ BlocoX.Test/BlocoX.Utils/Xml/ReducaoZ/Tags/ProdutosServicosTagTest.cs
@@ -0,0 +1,50 @@
+﻿using BlocoX.Modelos.ReducaoZ;
+using BlocoX.Test.BlocoXUtils.Fakes;
+using BlocoX.Utils.Xml.ReducaoZ.Tags;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace BlocoX.Test.BlocoXUtils.Xml.ReducaoZ.Tags
+{
+    [TestClass]
+    [TestCategory("Produtos - Validar Tags Produtos e Serviços")]
+    public class ProdutosServicosTagTest
+    {
+        [TestMethod]
+        public void Deve_validar_tag_produtos_servicos()
+        {
+            var produtoServicosTag = new ProdutosServicosTag(Informacoes.ObterListaProdutos());
+
+            Assert.AreEqual("<ProdutosServicos>" +
+                                "<Produto>" +
+                                    "<Descricao>PRODUTO ALIQUOTA 1700</Descricao>" +
+                                    "<CodigoGTIN></CodigoGTIN>" +
+                                    "<CodigoCEST>1400300</CodigoCEST>" +
+                                    "<CodigoNCMSH>11041900</CodigoNCMSH>" +
+                                    "<CodigoProprio>220601</CodigoProprio>" +
+                                    "<Quantidade>2,000</Quantidade>" +
+                                    "<Unidade>UN</Unidade>" +
+                                    "<ValorDesconto>0,00</ValorDesconto>" +
+                                    "<ValorAcrescimo>0,00</ValorAcrescimo>" +
+                                    "<ValorCancelamento>0,00</ValorCancelamento>" +
+                                    "<ValorTotalLiquido>1000,00</ValorTotalLiquido>" +
+                                "</Produto>" +
+                                "<Produto>" +
+                                    "<Descricao>PRODUTO ALIQUOTA 1700</Descricao>" +
+                                    "<CodigoGTIN></CodigoGTIN>" +
+                                    "<CodigoCEST>1400300</CodigoCEST>" +
+                                    "<CodigoNCMSH>11041900</CodigoNCMSH>" +
+                                    "<CodigoProprio>220601</CodigoProprio>" +
+                                    "<Quantidade>2,000</Quantidade>" +
+                                    "<Unidade>UN</Unidade>" +
+                                    "<ValorDesconto>0,00</ValorDesconto>" +
+                                    "<ValorAcrescimo>0,00</ValorAcrescimo>" +
+                                    "<ValorCancelamento>0,00</ValorCancelamento>" +
+                                    "<ValorTotalLiquido>1000,00</ValorTotalLiquido>" +
+                                "</Produto>" +
+                            "</ProdutosServicos>", produtoServicosTag.ObterTag().ToString());
+
+        }
+
+    }
+}
